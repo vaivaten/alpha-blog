ad32d57cbbecda07c1b7856c1748584d9761a964
Vaiva <vaivaa.t@gmail.com>
2018-05-28 11:33:03 +0300

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_081414) do
+ActiveRecord::Schema.define(version: 2018_05_28_083210) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
